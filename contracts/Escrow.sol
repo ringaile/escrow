7398de35a5549a7a3c73e2ae12b04e7989461b1b
Ringaile Valiaugaite <ringaile@gmail.com>
2021-08-07 18:02:38 +0200

    added expiration time for tokens

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -7,13 +7,18 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Escrow {
 
   address owner;
+  uint256 expiration;
 
   // assuming that there could be few tokens.
   // payee address => token address => amount
   mapping(address => mapping(address => uint256)) public deposits;
 
-  constructor() {
+  // payee address => token address => expiration time
+  mapping(address => mapping(address => uint256)) public expirations;
+
+  constructor(uint _expiration) {
       owner = msg.sender;
+      expiration = _expiration;
   }
 
   modifier onlyOwner() {
@@ -25,9 +30,11 @@ contract Escrow {
   function deposit(address _payee, IERC20 _token, uint256 _amount) public onlyOwner payable{
       require(_token.transferFrom(msg.sender, address(this), _amount));
       deposits[_payee][address(_token)] += _amount;
+      expirations[_payee][address(_token)] = block.timestamp + expiration;
   }
 
   function withdraw(address payable _payee, IERC20 _token) public onlyOwner{
+      require(block.timestamp > expirations[_payee][address(_token)], "The payment is still in escrow.");
       uint256 payment = deposits[_payee][address(_token)];
       deposits[_payee][address(_token)] = 0;
       require(_token.transfer(msg.sender, payment));
